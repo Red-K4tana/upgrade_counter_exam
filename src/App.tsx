f767e6211016d7bc1b27da2663873e89024bc5c1
Red-K4tana <ceptor.nill@gmail.com>
2021-12-28 14:07:12 +0300

    upgrade_counter_(in_process)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Counter} from "./Counter";
+import {Setting} from "./Setting";
+import {saveState} from "./LocalStarage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [maxValue, setMaxValue] = useState('5')
+    const [startValue, setStartValue] = useState('0')
+
+    const [counter, setCounter] = useState(Number(startValue))
+
+    const setValue = () => {
+        saveState('maxValue', maxValue)
+        saveState('startValue', startValue)
+        setCounter(Number(startValue))
+    }
+
+
+
+    return (
+        <div className="App">
+            <Setting setMaxValue={setMaxValue}
+                     setStartValue={setStartValue}
+                     maxValue={maxValue}
+                     startValue={startValue}
+                     setValue={setValue} />
+            <Counter setCounter={setCounter} counter={counter}/>
+        </div>
+    );
 }
 
 export default App;
