ddbbfb1924e4a808db55747ad29b41cf2eff0336
Red-K4tana <ceptor.nill@gmail.com>
2021-12-28 17:39:22 +0300

    upgrade_counter_(in_process)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import React, {useState} from 'react';
 import './App.css';
 import {Counter} from "./Counter";
 import {Setting} from "./Setting";
-import {saveState} from "./LocalStarage";
+import {restoreState, saveState} from "./LocalStarage";
 
 function App() {
     const [maxValue, setMaxValue] = useState('5')
     const [startValue, setStartValue] = useState('0')
-
     const [counter, setCounter] = useState(Number(startValue))
 
     const setValue = () => {
@@ -17,15 +16,28 @@ function App() {
     }
 
 
+    const disButton = () => {
+        if (Number(maxValue) <= Number(startValue)) {
+            return true
+        } else if (Number(maxValue) < 0 || Number(startValue) < 0) {
+            return true
+        } else {
+            return Number(maxValue) === Number(restoreState('maxValue')) && Number(startValue) === Number(restoreState('startValue'));
+        }
+    }
+
 
     return (
         <div className="App">
-            <Setting setMaxValue={setMaxValue}
-                     setStartValue={setStartValue}
-                     maxValue={maxValue}
-                     startValue={startValue}
-                     setValue={setValue} />
-            <Counter setCounter={setCounter} counter={counter}/>
+            <Setting
+                setMaxValue={setMaxValue}
+                setStartValue={setStartValue}
+                maxValue={maxValue}
+                startValue={startValue}
+                setValue={setValue}
+                disButton={disButton}
+            />
+            <Counter setCounter={setCounter} counter={counter} forInform={disButton}/>
         </div>
     );
 }
